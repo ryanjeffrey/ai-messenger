af046e63991b5526b7fbd22088565b001f234b39
ryanjeffrey <ryanjeffreysmith@gmail.com>
2023-02-09 19:45:36 -0600

    Add profile pic to bottom of sidebar component and sign out on click

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,20 +1,31 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
 import NewChat from "./NewChat";
 
 function SideBar() {
+  const { data: session } = useSession();
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
         <div>
-            <NewChat />
+          <NewChat />
 
-            <div>
-                {/* ModelSelection */}
-            </div>
+          <div>{/* ModelSelection */}</div>
 
-            {/* Map through the ChatRows */}
-
+          {/* Map through the ChatRows */}
         </div>
       </div>
+
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt="profile pic"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+        />
+      )}
     </div>
   );
 }
