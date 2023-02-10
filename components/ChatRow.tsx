d562a1fa15f0ff9ad5131c192e006c7796ed52d2
ryanjeffrey <ryanjeffreysmith@gmail.com>
2023-02-10 13:12:28 -0600

    Fetch the text from the messages collection and order by most recent

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,17 +1,38 @@
 import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { collection, orderBy, query } from "firebase/firestore";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
 
 type Props = {
-    id: string;
-}
+  id: string;
+};
 
-function ChatRow({id}: Props) {
+function ChatRow({ id }: Props) {
+  const pathname = usePathname();
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [active, setActive] = useState(false);
+
+  const [messages] = useCollection(
+    query(
+      collection(db, "users", session?.user?.email!, "chats", id, "messages"),
+      orderBy("createdAt", "asc")
+    )
+  );
+
   return (
     <Link href={`/chat/${id}`} className={`chatRow justify-center`}>
-        <ChatBubbleLeftIcon className="h-5 w-5" />
-        <p className="flex-1 hidden md:inline-flex truncate">New Chat</p>
-        <TrashIcon className="h-5 w-5 text-gray-700 hover:text-red-700" />
+      <ChatBubbleLeftIcon className="h-5 w-5" />
+      {/* Pull the last bit of text from the message or return "New Chat" */}
+      <p className="flex-1 hidden md:inline-flex truncate">
+        {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
+      </p>
+      <TrashIcon className="h-5 w-5 text-gray-700 hover:text-red-700" />
     </Link>
-  )
+  );
 }
-export default ChatRow
+export default ChatRow;
