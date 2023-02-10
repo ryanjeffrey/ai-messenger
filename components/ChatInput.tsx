badd27a977504b818f1680a6c6793557b8ef36fe
ryanjeffrey <ryanjeffreysmith@gmail.com>
2023-02-10 13:54:09 -0600

    Create chat input component

---
+++ components/ChatInput.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+
+type Props = {
+  chatId: string;
+};
+
+function ChatInput({ chatId }: Props) {
+  const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
+
+  return (
+    <div className="bg-gray-700/50 text-gray-400 rounded-lg">
+      <form className="p-5 space-x-5 flex">
+        <input
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          type="text"
+          placeholder="Type your message here..."
+          className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
+          disabled={!session}
+        />
+        <button type="submit" disabled={!prompt || !session} className="bg-[#11A37F] cursor-pointer hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed">
+          <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+        </button>
+      </form>
+
+      <div>{/* ModelSelection */}</div>
+    </div>
+  );
+}
+export default ChatInput;
